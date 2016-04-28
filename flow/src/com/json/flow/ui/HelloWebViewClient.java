36c1c87d85980f79812112219b1d9c0c55dad3ed
Administrator <Administrator@R1P78KGI7R4VPJB>
2016-04-28 15:39:19 +0800

    提交

--- flow/src/com/json/flow/ui/HelloWebViewClient.java
+++ flow/src/com/json/flow/ui/HelloWebViewClient.java
@@ -1,30 +1,53 @@
 package com.json.flow.ui;
 
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
+import java.util.ArrayList;
+
+import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class HelloWebViewClient extends WebViewClient{
-	
-	private Activity activity;
-	
+public class HelloWebViewClient extends WebViewClient {
+
+	static ArrayList<String> histories = new ArrayList<String>();
+
+	public static String CURRENT_URL = "";
+
+	private HomeActivity activity;
+
 	public HelloWebViewClient() {
-	
+
 	}
 
-	public HelloWebViewClient(Activity activty){
+	public HelloWebViewClient(HomeActivity activty) {
 		this.activity = activty;
 	}
-	
-	@Override 
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		if(!url.contains("deal")){
-			activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		}else{
-			activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+	@Override
+	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+		CURRENT_URL = url;
+		addUrl(url);;
+		view.loadUrl(url);
+		activity.requestedOrientation(url);
+		return true;
+	}
+
+	public static void addUrl(String url) {
+		Log.v("url addUrl", url);
+		histories.add(url);
+		if (histories.size() >= 50) {
+			//删除最后一个
+			histories.remove(histories.size()-1);
 		}
-        view.loadUrl(url);
-        return true;  
-    }  
+	}
+	
+	/***
+	 * 获取上一个url
+	 */
+	public static String getPreUrl(){
+		if(histories.size()>1){
+			Log.v("url getPreUrl ", histories.get(0));
+			return histories.get(0);
+		}
+		return null;
+	}
 }
--- flow/src/com/json/flow/ui/HomeActivity.java
+++ flow/src/com/json/flow/ui/HomeActivity.java
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.webkit.WebView;
 import android.widget.Toast;
 
 import com.json.flow.R;
@@ -31,35 +33,71 @@ public class HomeActivity extends Activity {
 			finish();
 		}
 		setContentView(R.layout.activity_home);
-		initView();
+		String url = UrlContant.URL_INDEX
+				+ UrlContant.getUri(sp.getString(UrlContant.SP_token, "NONE"));
+		url = "https://m.baidu.com/";
+		HelloWebViewClient.addUrl(url);
+		initView(url);
 	}
 
-	private void initView() {
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		try {
+			super.onConfigurationChanged(newConfig);
+			if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+				Log.v("Himi", "onConfigurationChanged_ORIENTATION_LANDSCAPE");
+			} else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+				Log.v("Himi", "onConfigurationChanged_ORIENTATION_PORTRAIT");
+			}
+		} catch (Exception ex) {
+		}
+	}
+
+	private void initView(String url) {
 		webview = (CustomWebView) findViewById(R.id.webview);
 		// 设置WebView属性，能够执行Javascript脚本
 		webview.getSettings().setJavaScriptEnabled(true);
-		webview.addJavascriptInterface(new JavaScriptinterface(HomeActivity.this), "android");
+		webview.addJavascriptInterface(new JavaScriptinterface(
+				HomeActivity.this), "android");
 		// 加载需要显示的网页
-		//String url = UrlContant.URL_INDEX
-			//	+ UrlContant.getUri(sp.getString(UrlContant.SP_token, "NONE"));
-		webview.loadUrl(UrlContant.URL_INDEX
-				+ UrlContant.getUri(sp.getString(UrlContant.SP_token, "NONE")));
+		webview.loadUrl(url);
 		webview.setWebViewClient(new HelloWebViewClient(HomeActivity.this));
 	}
 
 	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if ((System.currentTimeMillis() - mExitTime) > 2000) {
-			Toast.makeText(this,
-					getResources().getString(R.string.press_again_exit),
-					Toast.LENGTH_SHORT).show();
-			mExitTime = System.currentTimeMillis();
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK)
+				&& !HelloWebViewClient.CURRENT_URL.contains("m.baidu.com")) {
+			if (HelloWebViewClient.getPreUrl() != null) {
+				HelloWebViewClient.CURRENT_URL = HelloWebViewClient.getPreUrl();
+				requestedOrientation(HelloWebViewClient.getPreUrl());
+				initView(HelloWebViewClient.getPreUrl());
+			}
 			return true;
-		} else {
-			// 退出浏览器
-			// SplashActivity.removeView();
-			HomeActivity.this.finish();
+		} else if (keyCode == KeyEvent.KEYCODE_BACK
+				&& (HelloWebViewClient.CURRENT_URL.contains("m.baidu.com") || HelloWebViewClient.CURRENT_URL
+						.equals(""))) {
+			if ((System.currentTimeMillis() - mExitTime) > 2000) {
+				Toast.makeText(this,
+						getResources().getString(R.string.press_again_exit),
+						Toast.LENGTH_SHORT).show();
+				mExitTime = System.currentTimeMillis();
+				return true;
+			} else {
+				// 退出浏览器
+				// SplashActivity.removeView();
+				HomeActivity.this.finish();
+			}
 		}
-		return super.onKeyUp(keyCode, event);
+		return super.onKeyDown(keyCode, event);
 	}
+
+	public void requestedOrientation(String url) {
+		if (url.contains("/news")) {
+			this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		} else {
+			this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		}
+	}
+
 }
